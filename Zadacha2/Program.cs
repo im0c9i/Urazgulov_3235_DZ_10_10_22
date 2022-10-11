738ed4fee017023f7049a5ad131014ab57ec4a28
Ramil <ramil-urazgulov444@mail.ru>
2022-10-12 00:07:49 +0700

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+System.Console.WriteLine("Введите число ");
+int FerstNumber = int.Parse(Console.ReadLine());
+int count = 0;
+while (FerstNumber > 0)
+{
+    count = count + FerstNumber%10;
+    FerstNumber = FerstNumber/10;
+}
+Console.WriteLine($"Summ цифр в числе = {count}");

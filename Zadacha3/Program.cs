c66402a87d11adb895210925b8f1be7d67fd5482
Ramil <ramil-urazgulov444@mail.ru>
2022-10-12 00:22:12 +0700

    Zadacha4

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())*/
+Console.WriteLine("Введите массив: ");
+string text = Console.ReadLine();
+ 
+string[] words = text.Split(new char[] { ' ' });
+ 
+foreach (string s in words)
+{
+    Console.WriteLine($"->[{s}]");
+}
